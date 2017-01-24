934c912513f3bb64cafd510583e25444c34304b9
John Espenhahn <john@espenhahn.org>
2017-01-24 16:39:21 -0500

    Add more Parser tests

--- src/miniJava/test/ParserTest.java
+++ src/miniJava/test/ParserTest.java
@@ -16,10 +16,28 @@ public class ParserTest {
 	public void test() {
 		// CLASS DECLARATION
 		checkParse("class c { public static boolean b; }", WrapperType.NONE, true);
+		checkParse("class c { } class c2 { }", WrapperType.NONE, true);
+		checkParse("class c {} class", WrapperType.NONE, false);
+		checkParse("class c }", WrapperType.NONE, false);
 		// FIELD DECLARATION
 		checkParse("public static int log_id_123_a;", WrapperType.CLASS, true);
 		// METHOD DECLARATION
 		checkParse("public void function1(int P1, String p2) { }", WrapperType.CLASS, true);
+		checkParse("public f1() { }", WrapperType.CLASS, false);
+		checkParse("public private f1() { }", WrapperType.CLASS, false);
+		checkParse("f1() {}", WrapperType.CLASS, false);
+		checkParse("static f1() {}", WrapperType.CLASS, false);
+		checkParse("static private f1() {}", WrapperType.CLASS, false);
+		checkParse("static private Type f1() {}", WrapperType.CLASS, false);
+		checkParse("private static Type f1() {}", WrapperType.CLASS, true);
+		checkParse("private static f1() {}", WrapperType.CLASS, false);
+		checkParse("private static void f1() {}", WrapperType.CLASS, true);
+		checkParse("private void static f1() {}", WrapperType.CLASS, false);
+		checkParse("void private static f1() {}", WrapperType.CLASS, false);
+		checkParse("void f1()", WrapperType.CLASS, false);
+		checkParse("void f1 {}", WrapperType.CLASS, false);
+		checkParse("Type f1()", WrapperType.CLASS, false);
+		checkParse("Type f1 {}", WrapperType.CLASS, false);
 		// VISIBILITY
 		checkParse("private void f() {}", WrapperType.CLASS, true);
 		checkParse("public void f() { }", WrapperType.CLASS, true);
@@ -27,22 +45,95 @@ public class ParserTest {
 		checkParse("static void f() { }", WrapperType.CLASS, true);
 		// TYPE
 		checkParse("int i; boolean b; SomeOtherClass c; int[] ints; Another123[] objs;", WrapperType.CLASS, true);
+		checkParse("int i", WrapperType.CLASS, false);
+		checkParse("int[] i", WrapperType.CLASS, false);
+		checkParse("boolean i", WrapperType.CLASS, false);
+		checkParse("Type i", WrapperType.CLASS, false);
+		checkParse("int i;", WrapperType.CLASS, true);
+		checkParse("int[] i;", WrapperType.CLASS, true);
+		checkParse("boolean i;", WrapperType.CLASS, true);
+		checkParse("Type i;", WrapperType.CLASS, true);
 		// PARAM LIST
 		checkParse("void function(int id1, boolean id2, String id3) {}", WrapperType.CLASS, true);
+		checkParse("Type func(int a) {}", WrapperType.CLASS, true);
+		checkParse("Type func(int a, int b) {}", WrapperType.CLASS, true);
+		checkParse("Type func(int a.j, int b) {}", WrapperType.CLASS, false);
+		checkParse("Type func(int a, j, int b) {}", WrapperType.CLASS, false);
+		checkParse("Type func(int a, int b) {} {}", WrapperType.CLASS, false);
+		checkParse("Type func(int a, int (b)) {}", WrapperType.CLASS, false);
+		checkParse("Type func(int a, (int b)) {}", WrapperType.CLASS, false);
+		checkParse("Type[] func(int a, int[] j, int b) {}", WrapperType.CLASS, true);
+		// ARGLIST
+		checkParse("f1(b, b[0], b.c, b.c.d, b.c[2].d, 1, 1/2, 2*3/1);", WrapperType.FUNCTION, true);
+		checkParse("f1(b, b[0], b.c, b.c.d, b.c[2].d, 1, 1/2, 2*3/1)", WrapperType.FUNCTION, false);
 		// REFERENCE
 		checkParse("identifier[1+2-3/4*5]", WrapperType.RESULT, true);
 		checkParse("this", WrapperType.RESULT, true);
+		checkParse("1cd.b = 1;", WrapperType.FUNCTION, false);
+		checkParse("_1cd.b = 1;", WrapperType.FUNCTION, false);
+		checkParse("a_1cd.b = 1;", WrapperType.FUNCTION, true);
 		checkParse("ref.ref2[1<2]", WrapperType.CONDITIONAL, true);
+		checkParse("ref.ref2[1<2] = 1;", WrapperType.FUNCTION, true);
+		checkParse("ref.ref2[1<2] = 1", WrapperType.FUNCTION, false);
+		checkParse("ref.ref2[1<2] = bc_d1__d;", WrapperType.FUNCTION, true);
 		checkParse("this.c.k = 1;", WrapperType.FUNCTION, true);
+		checkParse("this.c.k = 1", WrapperType.FUNCTION, false);
 		checkParse("this.c[b.d.f[1].a].k = 1;", WrapperType.FUNCTION, true);
+		checkParse("this.c[b.d.f[1].a].k = 1", WrapperType.FUNCTION, false);
 		// STATEMENT
 		checkParse("return \n\t 1 <= 2;", WrapperType.FUNCTION, true);
 		checkParse("if (1 == 2) func(); else func2();", WrapperType.FUNCTION, true);
+		checkParse("if (1 == 2) func(); else func2()", WrapperType.FUNCTION, false);
+		checkParse("if (1 == 2) func() else func2();", WrapperType.FUNCTION, false);
+		checkParse("if (1 == 2) func();", WrapperType.FUNCTION, true);
+		checkParse("if (1 == 2) { func(); }", WrapperType.FUNCTION, true);
+		checkParse("if (1 == 2) { func(); } else { func2(); }", WrapperType.FUNCTION, true);
 		checkParse("while (id1 && id2 || id3 && !id4 || id5 != id6) { }", WrapperType.FUNCTION, true);
+		checkParse("while (id1 && id2 || id3 && !id4 || id5 != id6) { } else {}", WrapperType.FUNCTION, false);
+		checkParse("while (id1 && id2 || id3 && !id4 || id5 != id6) { };", WrapperType.FUNCTION, false);
+		checkParse("int i = 0;", WrapperType.FUNCTION, true);
+		checkParse("int i j = 0;", WrapperType.FUNCTION, false);
+		checkParse("int i = 0; i j = 0;", WrapperType.FUNCTION, true);
+		checkParse("int i = i j = 0;", WrapperType.FUNCTION, false);
+		checkParse("int i = i = 0;", WrapperType.FUNCTION, false);
+		checkParse("int[] i = j;", WrapperType.FUNCTION, true);
+		checkParse("int[] i = j[1];", WrapperType.FUNCTION, true);
+		checkParse("int[] i = j[];", WrapperType.FUNCTION, false);
+		checkParse("this = 1;", WrapperType.FUNCTION, true);
+		checkParse("this();", WrapperType.FUNCTION, true);
+		checkParse("Type id = new Type();", WrapperType.FUNCTION, true);
+		checkParse("id = new Type();", WrapperType.FUNCTION, true);
+		checkParse("id();", WrapperType.FUNCTION, true);
+		checkParse("this = 1", WrapperType.FUNCTION, false);
+		checkParse("this()", WrapperType.FUNCTION, false);
+		checkParse("Type id = new Type()", WrapperType.FUNCTION, false);
+		checkParse("id = new Type()", WrapperType.FUNCTION, false);
+		checkParse("id()", WrapperType.FUNCTION, false);
+		checkParse("{}", WrapperType.FUNCTION, true);
+		checkParse("{ int i = 0; }", WrapperType.FUNCTION, true);
+		checkParse("{ int i = 0; int j = 1; }", WrapperType.FUNCTION, true);
+		checkParse("{ int i = 0; int j = 1; int k = 2; }", WrapperType.FUNCTION, true);
 		// EXPRESSION
 		checkParse("return true; return false;", WrapperType.FUNCTION, true);
+		checkParse("return true", WrapperType.FUNCTION, false);
 		checkParse("id myid = new id(); int[] ints = 1+!1;", WrapperType.FUNCTION, true);
 		checkParse("(1 > 2)", WrapperType.CONDITIONAL, true);
+		checkParse("(1 > 2 < 3)", WrapperType.CONDITIONAL, true);
+		checkParse("(1 > 2 < 3 == 4)", WrapperType.CONDITIONAL, true);
+		checkParse("(1 > 2 < 3 == 4 + 5)", WrapperType.CONDITIONAL, true);
+		checkParse("(1 > 2 < 3 == 4 ++ 5)", WrapperType.CONDITIONAL, false);
+		checkParse("(1 > (2 < 3))", WrapperType.CONDITIONAL, true);
+		checkParse("((1 > 2) < 3)", WrapperType.CONDITIONAL, true);
+		checkParse("((1 > 2) < !3)", WrapperType.CONDITIONAL, true);
+		checkParse("((1 > --2) < !3)", WrapperType.CONDITIONAL, true);
+		checkParse("((1 -> --2) < !3)", WrapperType.CONDITIONAL, false);
+		checkParse("((1 => --2) < !3)", WrapperType.CONDITIONAL, false);
+		checkParse("((1 => --2) = 3)", WrapperType.CONDITIONAL, false);
+		checkParse("((1 >= --2) == 3)", WrapperType.CONDITIONAL, true);
+		checkParse("((1 >= --2) => 3)", WrapperType.CONDITIONAL, false);
+		checkParse("((1 >= --2) == (3))", WrapperType.CONDITIONAL, true);
+		checkParse("1 == ()", WrapperType.CONDITIONAL, false);
+		checkParse("1 == (true)", WrapperType.CONDITIONAL, true);
 		checkParse("int i = 0;", WrapperType.FUNCTION, true);
 		checkParse("boolean b = i;", WrapperType.FUNCTION, true);
 		// COMMENT
