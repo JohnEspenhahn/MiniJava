31ab3266e928bf107a83a756572db8135be3f6e5
John Espenhahn <john@espenhahn.org>
2017-02-13 11:35:53 -0500

    Add missing debug mode positionless constructor

--- src/miniJava/AbstractSyntaxTrees/ArrayType.java
+++ src/miniJava/AbstractSyntaxTrees/ArrayType.java
@@ -10,6 +10,10 @@ import miniJava.SyntacticAnalyzer.SourcePosition;
 
 public class ArrayType extends TypeDenoter {
 
+	public ArrayType(TypeDenoter eltType) {
+		this(eltType, SourcePosition.ZERO);
+	}
+	
 	    public ArrayType(TypeDenoter eltType, SourcePosition posn){
 	        super(TypeKind.ARRAY, posn);
 	        this.eltType = eltType;
